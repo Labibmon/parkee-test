6c1da90d475377af421f2d2f2726edf34e231663
labib <labibmon@gmail.com>
2026-01-28 22:32:52 +0700

    fix init: base app and styled-componen

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,3 +1,4 @@
+import Button from "~/components/shared/Button";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
@@ -6,7 +7,7 @@ export function Welcome() {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
+          <div className="w-125 max-w-[100vw] p-4">
             <img
               src={logoLight}
               alt="React Router"
@@ -19,7 +20,7 @@ export function Welcome() {
             />
           </div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
+        <div className="max-w-75 w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
@@ -42,6 +43,12 @@ export function Welcome() {
           </nav>
         </div>
       </div>
+
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="danger">Delete</Button>
+      <Button size="sm">Small</Button>
+      <Button fullWidth>Submit</Button>
     </main>
   );
 }
